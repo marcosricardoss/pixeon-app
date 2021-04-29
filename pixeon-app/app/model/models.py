38d13fc9fe20e5e60f047e5f1885c65e59005551
Marcos Ricardo <marcosricardoss@gmail.com>
2021-04-29 16:38:51 -0300

    feat: add models

--- pixeon-app/app/model/models.py
+++ pixeon-app/app/model/models.py
@@ -73,3 +73,145 @@ class User(Base, Model, CreationModificationDateMixin):
 
     def __repr__(self) -> str:
         return '<User %r>' % (self.username)
+
+
+class Physician(Base, Model, CreationModificationDateMixin):
+    """ Physician's model class.
+    
+    Column:
+    id              (interger, primary key)
+    public_id*      (str): public ID
+    name*           (string)
+    
+    """
+
+    __tablename__ = 'physician'
+
+    id = Column(Integer, primary_key=True)
+    public_id = Column(String(50), unique=True, nullable=False)
+    name = Column(String(250), nullable=False)    
+
+    def serialize(self, timezone=UTC) -> dict:
+        """Serialize the object attributes values into a dictionary.
+
+        Returns:
+        dict: a dictionary containing the attributes values
+        """
+
+        data = {
+            "public_id": self.public_id,
+            'name': self.name,
+            "created_at": self.created_at.astimezone(timezone).isoformat(),
+            "updated_at": self.updated_at.astimezone(timezone).isoformat() if self.updated_at else ''            
+        }
+
+        return data
+
+    def __repr__(self) -> str:
+        return '<Physician %r>' % (self.name)       
+
+
+class Patient(Base, Model, CreationModificationDateMixin):
+    """ Patient's model class.
+    
+    Column:
+    id              (interger, primary key)
+    public_id*      (str): public ID
+    name*           (string)
+    
+    """
+
+    __tablename__ = 'patient'
+
+    id = Column(Integer, primary_key=True)
+    public_id = Column(String(50), unique=True, nullable=False)
+    name = Column(String(250), nullable=False)    
+
+    def serialize(self, timezone=UTC) -> dict:
+        """Serialize the object attributes values into a dictionary.
+
+        Returns:
+        dict: a dictionary containing the attributes values
+        """
+
+        data = {
+            "public_id": self.public_id,
+            'name': self.name,
+            "created_at": self.created_at.astimezone(timezone).isoformat(),
+            "updated_at": self.updated_at.astimezone(timezone).isoformat() if self.updated_at else ''            
+        }
+
+        return data
+
+    def __repr__(self) -> str:
+        return '<Patient %r>' % (self.name)
+
+
+class Order(Base, Model, CreationModificationDateMixin):
+    """ User's model class.
+    
+    Column:
+    id              (interger, primary key)
+    public_id*      (str): public ID
+    name*           (string)
+    
+    """
+
+    __tablename__ = 'order'
+
+    id = Column(Integer, primary_key=True)
+    public_id = Column(String(50), unique=True, nullable=False)    
+
+    def serialize(self, timezone=UTC) -> dict:
+        """Serialize the object attributes values into a dictionary.
+
+        Returns:
+        dict: a dictionary containing the attributes values
+        """
+
+        data = {
+            "public_id": self.public_id,            
+            "created_at": self.created_at.astimezone(timezone).isoformat(),
+            "updated_at": self.updated_at.astimezone(timezone).isoformat() if self.updated_at else ''            
+        }
+
+        return data
+
+
+class Exam(Base, Model, CreationModificationDateMixin):
+    """ User's model class.
+    
+    Column:
+    id              (interger, primary key)
+    public_id*      (str): public ID
+    name*           (string)
+    
+    """
+
+    __tablename__ = 'exam'
+
+    id = Column(Integer, primary_key=True)
+    public_id = Column(String(50), unique=True, nullable=False)
+    name = Column(String(250), nullable=False)    
+
+    def serialize(self, timezone=UTC) -> dict:
+        """Serialize the object attributes values into a dictionary.
+
+        Returns:
+        dict: a dictionary containing the attributes values
+        """
+
+        data = {
+            "public_id": self.public_id,
+            'name': self.name,
+            "created_at": self.created_at.astimezone(timezone).isoformat(),
+            "updated_at": self.updated_at.astimezone(timezone).isoformat() if self.updated_at else ''            
+        }
+
+        return data
+
+    def __repr__(self) -> str:
+        return '<Exam %r>' % (self.name)         
+
+
+  
